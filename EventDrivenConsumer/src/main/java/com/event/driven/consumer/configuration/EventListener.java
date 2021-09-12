24ab9408d495fa8c3912285373f2ae1af8af4351
Om Bijawat <omprakash-iiim@users.noreply.github.com>
2021-09-12 13:03:12 +0530

    Update EventListener.java

--- EventDrivenConsumer/src/main/java/com/event/driven/consumer/configuration/EventListener.java
+++ EventDrivenConsumer/src/main/java/com/event/driven/consumer/configuration/EventListener.java
@@ -14,7 +14,7 @@ public class EventListener {
 
     @JmsListener(destination = "event-queue")
     public void listener(String message){
-        logger.info("Event Type received", message);
+        logger.info("Event Type received : ", message);
     }
 	
 }
